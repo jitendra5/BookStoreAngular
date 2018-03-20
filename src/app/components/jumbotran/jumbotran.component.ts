5a6435aa8947ad0d9146ad14716f7bd1e9970bc8
jitendra5 <jitendra.jjr@gmail.com>
2018-03-20 21:36:13 +0530

    added style

--- src/app/components/jumbotran/jumbotran.component.ts
+++ src/app/components/jumbotran/jumbotran.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'jumbo',
   templateUrl: './jumbotran.component.html',
- 
+    styleUrls: ['./jumbotran.component.css']
+
 })
 export class JumbotranComponent {
   title = 'app';
