e5cc194fa4ca613ab1a638fb362b49c86777d896
jitendra5 <jitendra.jjr@gmail.com>
2018-03-21 01:43:01 +0530

    added new route for volumes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,12 +4,13 @@ import { ModuleWithProviders }  from '@angular/core';
 import {JumbotranComponent} from './components/jumbotran/jumbotran.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {SignupComponent} from './components/signup/signup.component';
+import {VolumesComponent} from './components/volumes/volumes.component';
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path:'home', component:JumbotranComponent},
   {path:'contact', component:ContactComponent},
   {path:'signup', component:SignupComponent},
-  
+  {path:'volumes', component:VolumesComponent},
 ];
 
 export const routing: ModuleWithProviders = RouterModule.forRoot(routes);
