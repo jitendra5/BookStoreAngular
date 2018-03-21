8e1e1f7425de920dc0ac33ccf869b22bc1593d0f
jitendra5 <jitendra.jjr@gmail.com>
2018-03-21 11:41:22 +0530

    injected service into the jambotrancomp

--- src/app/components/jumbotran/jumbotran.component.ts
+++ src/app/components/jumbotran/jumbotran.component.ts
@@ -1,10 +1,23 @@
 import { Component } from '@angular/core';
+import {GoogleBookService} from '../../services/googlebooks.service';
 @Component({
   selector: 'jumbo',
   templateUrl: './jumbotran.component.html',
-    styleUrls: ['./jumbotran.component.css']
+    styleUrls: ['./jumbotran.component.css'],
+    providers:[GoogleBookService]
 
 })
 export class JumbotranComponent {
-  title = 'app';
+  searchStr:string;
+  constructor(private googleBookService:GoogleBookService){
+
+  }
+  searchBooks(){
+    this.googleBookService.searchBook(this.searchStr)
+    .subscribe(res=>{
+      console.log(res.items);
+
+
+    });
+  }
 }
