126bb63b3310fbb1dc7ff1382582db7707a716a8
jitendra5 <jitendra.jjr@gmail.com>
2018-03-21 17:02:34 +0530

    import for Book model

--- src/app/components/jumbotran/jumbotran.component.ts
+++ src/app/components/jumbotran/jumbotran.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import {GoogleBookService} from '../../services/googlebooks.service';
+import {Book} from '../../../Book';
 @Component({
   selector: 'jumbo',
   templateUrl: './jumbotran.component.html',
@@ -9,15 +10,15 @@ import {GoogleBookService} from '../../services/googlebooks.service';
 })
 export class JumbotranComponent {
   searchStr:string;
+  results:Book[];
   constructor(private googleBookService:GoogleBookService){
 
   }
   searchBooks(){
     this.googleBookService.searchBook(this.searchStr)
     .subscribe(res=>{
+      this.results=res.items;
       console.log(res.items);
-
-
     });
   }
 }
